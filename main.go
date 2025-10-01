ab7f76d4db6824d8984b26548830df3ff5128b39
Zayyan Abdillah <muhammad.abdillah@ist.id>
2025-10-01 10:31:37 +0700

    profit calculator

--- main.go
+++ main.go
@@ -1,54 +1,25 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func getAlbumById(c *gin.Context) {
-	id := c.Param("id")
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+import "fmt"
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumById)
-	router.POST("/albums", postAlbums)
+	var revenue float64
+	var expenses float64
+	var taxRate float64
 
-	router.Run("localhost:8080")
+	fmt.Print("How much your revenue?")
+	fmt.Scan(&revenue)
 
+	fmt.Print("How much your expenses?")
+	fmt.Scan(&expenses)
+
+	fmt.Print("How much your tax rate?")
+	fmt.Scan(&taxRate)
+
+	earningBeforeTax := revenue - expenses
+	earningAfterTax := earningBeforeTax * (1 - taxRate/100)
+	ratio := earningBeforeTax / earningAfterTax
+	fmt.Println("EBT =", earningBeforeTax)
+	fmt.Println("EAT =", earningAfterTax)
+	fmt.Println("RATION =", ratio)
 }
