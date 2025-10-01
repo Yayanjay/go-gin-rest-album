069d130ead3e83a050242b5e49a0f2c7a1ef6cee
Zayyan Abdillah <muhammad.abdillah@ist.id>
2025-10-01 15:47:15 +0700

    minimum functional

--- main.go
+++ main.go
@@ -1,54 +1,57 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
+func main() {
+	var accountBalance = 1000.0
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
+	fmt.Println("Welcome to Go Bank")
+	for i := 0; i < 10; i++ {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
+		fmt.Print("Please choose what you want to do: ")
+		var input int
+		fmt.Println("your choice is", input)
+		fmt.Scan(&input)
 
-func getAlbumById(c *gin.Context) {
-	id := c.Param("id")
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
+		if input == 1 {
+			fmt.Println("your balance is ", accountBalance)
+		} else if input == 2 {
+			var inputDeposit float64
+			fmt.Print("how much do you want to deposit? ")
+			fmt.Scan(&inputDeposit)
+			if inputDeposit <= 0 {
+				fmt.Println("you need to deposit grater than zero")
+				continue
+			}
+			accountBalance += inputDeposit
+			fmt.Println("Balance updated, your current balance is: ", accountBalance)
+		} else if input == 3 {
+			var inputWithdraw float64
+			fmt.Print("how much do you want to withdraw? ")
+			fmt.Scan(&inputWithdraw)
+			if inputWithdraw <= 0 {
+				fmt.Println("you need to deposit grater than zero")
+				continue
+			}
+
+			if inputWithdraw > accountBalance {
+				fmt.Println("you can't withdraw more than you have")
+				continue
+			}
+			accountBalance -= inputWithdraw
+			fmt.Println("Balance updated, your current balance is: ", accountBalance)
+		} else {
+			fmt.Println("See ya")
+			break
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumById)
-	router.POST("/albums", postAlbums)
-
-	router.Run("localhost:8080")
+	fmt.Println("Thanks for using our bank")
 
 }
