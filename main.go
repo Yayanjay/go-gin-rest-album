6c1ce977b5d5a5e0b52b49767890f73cde9b4d77
Zayyan Abdillah <muhammad.abdillah@ist.id>
2025-10-01 16:15:00 +0700

    get amount balance from file

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64 {
+	data, _ := os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance, _ := strconv.ParseFloat(balanceText, 64)
+	return balance
+}
 
 func writeBalanceToFile(balance float64) {
 	balanceText := fmt.Sprint(balance)
-	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
 }
 func main() {
-	var accountBalance = 1000.0
+	var accountBalance = getBalanceFromFile()
 
 	fmt.Println("Welcome to Go Bank")
 	for i := 0; i < 10; i++ {
@@ -21,7 +31,7 @@ func main() {
 		fmt.Println("4. Exit")
 
 		var input int
-		fmt.Print("your choice is")
+		fmt.Print("your choice is ")
 		fmt.Scan(&input)
 
 		switch input {
